5417dfc1d83b9d1e9579e9eb1eaea401781ccf59
Kalinkin Yaroslav <kalinkin.y.o@mail.ru>
2024-09-06 11:06:07 +0300

    1.2

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from config_data.config import load_config
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers import user_handlers, other_handlers
 
 
-config = load_config('D:/GitHub/Repositories/tg_bot_template/.env')
+# Функция конфигурирования и запуска бота
+async def main():
 
-# Сохраняем токен в переменную bot_token
-bot_token = config.tg_bot.token
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
 
-# Сохраняем ID админа в переменную superadmin
-superadmin = config.tg_bot.admin_ids[0]
+    # Инициализируем бот
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
 
-print(bot_token, '\n', superadmin)
+    # Регистрируем роутеры в диспечере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
